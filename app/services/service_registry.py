bcbaadc2b818853c3fff94ba9739cac8a8d6e56c
Akshay Patel <akshayrpatel24@gmail.com>
2025-11-27 19:31:23 -0500

    update vectordb intialization as per app mode

--- app/services/service_registry.py
+++ app/services/service_registry.py
@@ -40,7 +40,9 @@ async def init_services() -> None:
 	logger.info("[ServiceRegistry] Initializing services")
 	service_registry.llm = LLMService()
 	service_registry.memory = MemoryService()
-	service_registry.vectordb = VectorDBService(mode=vectordb_config.mode)
+	service_registry.vectordb = VectorDBService(
+		mode=vectordb_config.mode,
+		ssl=vectordb_config.ssl)
 	service_registry.rag = RAGService(
 		memory=service_registry.memory,
 		vectordb=service_registry.vectordb,
