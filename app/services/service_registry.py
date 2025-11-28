5855738624af3337ecae730c4b41f3b739591b07
Akshay Patel <akshayrpatel24@gmail.com>
2025-11-27 19:37:03 -0500

    update vectordb intialization args

--- app/services/service_registry.py
+++ app/services/service_registry.py
@@ -42,6 +42,8 @@ async def init_services() -> None:
 	service_registry.memory = MemoryService()
 	service_registry.vectordb = VectorDBService(
 		mode=vectordb_config.mode,
+		host=vectordb_config.host,
+		port=vectordb_config.port,
 		ssl=vectordb_config.ssl)
 	service_registry.rag = RAGService(
 		memory=service_registry.memory,
